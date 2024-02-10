7cc1e77a1b352ded5572e450dd822779e143e01e
Oliwia <106619574+okrzanowska@users.noreply.github.com>
2024-02-10 14:05:53 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace PoruszanieSiePoPliku
+{
+    class Program
+    {
+        private static void Main()
+        {
+            const string filePath = "biodata.txt";
+            const string content = "Sekwencja kwasów nukleinowych: 5-AATTGGCC-3";
+
+            File.WriteAllText(filePath, content);
+
+            using (var fs = new FileStream(filePath, FileMode.Open))
+            {
+                fs.Seek(32, SeekOrigin.Begin);
+
+                using (var sr = new StreamReader(fs, Encoding.UTF8))
+                {
+                    Console.WriteLine(sr.ReadToEnd());
+                }
+            }
+
+            File.Delete(filePath);
+            Console.Clear();
+        }
+    }
+}
